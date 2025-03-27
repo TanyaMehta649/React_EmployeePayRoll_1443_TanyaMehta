e3fcb87b38acf680751080b0bd54841f9c2756ce
TanyaMehta649 <tanyamehta649@gmail.com>
2025-03-27 19:29:42 +0530

    [Tanya]:Added jest.config.cjs

---
+++ jest.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'jsdom', 
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.js', 
+    '@testing-library/jest-dom' 
+  ],
+  setupFiles: [], 
+  moduleNameMapper: {
+    
+    '^.+\\.(css|less|scss|png|jpg|jpeg|gif|svg)$': 'identity-obj-proxy'
+  },
+  transform: {
+    
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  collectCoverage: true, 
+  coverageDirectory: 'coverage', 
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'], 
+  coverageReporters: ['lcov']
+};
+
